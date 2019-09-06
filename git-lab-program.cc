9fbb18329c5bb6ea2c1521e275a50ba3044f49cc
Xiangsi Howard <xiangsih2000@gmail.com>
2019-09-06 15:23:04 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <XiangSi Howard>
+ *        Date: <September 6th 2019>
+ * Description: 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!!" << endl;
+
+    return 0;
+}// main
+
